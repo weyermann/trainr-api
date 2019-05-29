65675351d8c84be0171e865a1c3e8b77092d0705
Joerg Weyermann <joerg.weyermann@estv.admin.ch>
2019-05-29 15:48:08 +0200

    handler for workoutExecutions

---
+++ app/handler/workoutExecutions.go
@@ -0,0 +1,49 @@
+package handler
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	"github.com/weyermann/trainr-api/app/model"
+)
+
+// GetAllExecutions returns all sessions of a user
+func GetAllExecutions(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	executions := []model.WorkoutExecution{}
+
+	vars := mux.Vars(r)
+	sessionID := vars["sessionID"]
+
+	// Get all sessions matching the user
+	db.Where("sessionID = ?", sessionID).Find(&executions)
+	//// SELECT * FROM users WHERE sessionID = 'xyz';
+
+	respondJSON(w, http.StatusOK, executions)
+}
+
+// GetExecution returns a workout by given executionID
+func GetExecution(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	executionID, err := strconv.Atoi(vars["executionID"])
+	if err != nil {
+		return
+	}
+	execution := getExecutionOr404(db, executionID, w, r)
+	if execution == nil {
+		return
+	}
+	respondJSON(w, http.StatusOK, execution)
+}
+
+// getSessionOr404 gets a session instance if exists, or respond the 404 error otherwise
+func getExecutionOr404(db *gorm.DB, executionID int, w http.ResponseWriter, r *http.Request) *model.WorkoutExecution {
+	execution := model.WorkoutExecution{}
+	if err := db.First(&execution, model.WorkoutExecution{ID: executionID}).Error; err != nil {
+		respondError(w, http.StatusNotFound, err.Error())
+		return nil
+	}
+	return &execution
+}
